2599e76085ceedb3e13d4db3098096502c9a56e5
aartaka <aartaka@protonmail.com>
2022-09-11 14:50:19 +0400

    functions(jget): Allow nested indexing.

--- functions.lisp
+++ functions.lisp
@@ -4,21 +4,46 @@
 (in-package #:njson)
 
 (defgeneric jget (key-or-index object)
+  (:method ((keys sequence) (object t))
+    (reduce (lambda (data key) (jget key data))
+            keys :initial-value object))
   (:method ((index integer) (object sequence))
     (elt object index))
-  (:method (key (object hash-table))
+  (:method ((key string) (object hash-table))
     (gethash key object))
   (:documentation "Get the value at KEY-OR-INDEX in OBJECT.
+
+KEY-OR-INDEX can be
+- an integer (for array indexing),
+- a string (for object keying),
+- or a sequence of integers and strings (to index the nested
+  structures).
+
+For example, to get the data from a structure like
+{\"data\": [1, 2, {\"three\": 3}]}
+you can use
+(jget #(\"data\" 2 \"three\") data)
+;; => 3
 
 OBJECT can be JSON array or object, which in Lisp translates to any
 valid `sequence' ot `hash-table'."))
 
 (defgeneric (setf jget) (value key-or-index object)
+  (:method (value (keys sequence) (object t))
+    (setf (jget (elt keys (1- (length keys)))
+                (jget (subseq keys 0 (1- (length keys))) object))
+          value))
   (:method (value (index integer) (object sequence))
     (setf (elt object index) value))
-  (:method (value key (object hash-table))
+  (:method (value (key string) (object hash-table))
     (setf (gethash key object) value))
   (:documentation "Set the value at KEY-OR-INDEX in OBJECT.
+
+KEY-OR-INDEX can be
+- an integer (for array indexing),
+- a string (for object keying),
+- or a sequence of integers and strings (to modify the nested
+  structures).
 
 OBJECT can be JSON array or object, which in Lisp translates to any
 valid `sequence' ot `hash-table'."))
@@ -29,16 +54,13 @@ valid `sequence' ot `hash-table'."))
 OBJECT can be JSON array or object, which in Lisp translates to any
 valid `sequence' ot `hash-table'.
 
-For generic implementation, see `jget'."
+For generic implementation and getails, see `jget'."
   (jget key-or-index object))
 
 (defun (setf get**) (value key-or-index object)
   "Set the value at KEY-OR-INDEX in OBJECT.
 
-OBJECT can be JSON array or object, which in Lisp translates to any
-valid `sequence' ot `hash-table'.
-
-For generic implementation, see `jget'."
+For generic implementation and getails, see `jget'."
   (setf (jget key-or-index object) value))
 
 (defgeneric jtruep (object)
