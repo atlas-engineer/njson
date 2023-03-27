1fa9bfc351da0bc87dac267427a56c41d817f8e4
Artyom Bologov <aartaka@protonmail.com>
2023-03-27 12:26:54 +0400

    tests: Adjust to vector change.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -19,8 +19,8 @@
 (define-test tricky-values ()
   (assert-typep 'hash-table (decode "{}"))
   (assert-eql 0 (hash-table-count (decode "{}")))
-  (assert-typep 'sequence (decode "[]"))
-  (assert-equalp '(nil) (decode "[false]")))
+  (assert-typep 'vector (decode "[]"))
+  (assert-equalp #(nil) (decode "[false]")))
 
 ;;; TODO: jget, jrem, jhas tests.
 
@@ -28,7 +28,7 @@
   (destructuring-bind (simple-1 float-3.8 true false undefined null
                        string-foo array-123 array-of-everything
                        object-quux-1883 object-of-everything)
-      (decode (asdf:system-relative-pathname :njson "tests/test.json"))
+      (coerce (decode (asdf:system-relative-pathname :njson "tests/test.json")) 'list)
     (assert-eql 1 simple-1)
     (assert-eql 3.8 float-3.8)
     (assert-eq t true)
@@ -36,18 +36,18 @@
     (assert-eq :undefined undefined)
     (assert-eq :null null)
     (assert-equal "foo" string-foo)
-    (assert-equalp '(1 2 3) array-123)
-    (assert-equalp '("bar" 8.3 t :null 1000000) array-of-everything)
+    (assert-equalp #(1 2 3) array-123)
+    (assert-equalp #("bar" 8.3 t :null 1000000) array-of-everything)
     (assert-typep 'hash-table object-quux-1883)
     (assert-eql 1883 (jget "quux" object-quux-1883))
     (assert-typep 'hash-table object-of-everything)
     (assert-eql 1 (jget "foo" object-of-everything))
-    (assert-equalp '(1 2 "hey") (jget "bar" object-of-everything))
+    (assert-equalp #(1 2 "hey") (jget "bar" object-of-everything))
     (assert-typep 'hash-table (jget "quux" object-of-everything))
     (assert-eql 1 (jget "one" (jget "quux" object-of-everything)))))
 
 (define-test jcopy-test ()
-  (let* ((list '(1 2 3 "hello")))
+  (let* ((array #(1 2 3 "hello")))
     (assert-eql 8 (jcopy 8))
     (assert-eql 1.3 (jcopy 1.3))
     (assert-eq :null (jcopy :null))
@@ -57,5 +57,5 @@
     (assert-false (jcopy nil))
     (assert-equal "hello there" (jcopy "hello there"))
     ;; TODO: hash-tables
-    (assert-false (eq list (jcopy list)))
-    (assert-equal list (jcopy list))))
+    (assert-false (eq array (jcopy array)))
+    (assert-equalp array (jcopy array))))
