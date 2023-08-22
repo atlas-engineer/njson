75984de8ea29d8fe1291c534d7e92b506964dc72
Artyom Bologov <aartaka@protonmail.com>
2023-08-22 15:02:02 +0400

    backend/cl-json: Allow comments in JSON.
    Some JSON files (like WebExtensions' manifest.json) allow '//'
    comments. It breaks cl-json, thus the redefinition of its core
    functions.

--- backend/cl-json.lisp
+++ backend/cl-json.lisp
@@ -23,6 +23,47 @@ Objects are transformed to the hash-tables instead.")
 (defun json-object-get ()
   *json-object-accumulator*)
 
+(defun json::read-json-token (stream)
+  "Read a JSON token (literal name, number or punctuation char) from
+the given STREAM, and return 2 values: the token category (a symbol)
+and the token itself, as a string or character."
+  (let ((c (peek-char t stream)))
+    (case c
+      ((#\{ #\[ #\] #\} #\" #\: #\,)
+       (values :punct (read-char stream)))
+      ((#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\-)
+       (json::read-json-number-token stream))
+      ;; Modified to ignore comments.
+      ((#\/)
+       (read-line stream)
+       (json::read-json-token stream))
+      (t (if (alpha-char-p c)
+             (json::read-json-name-token stream)
+             (json:json-syntax-error stream "Invalid char on JSON input: `~C'"
+                                c))))))
+
+(defun peek-json-token (stream)
+  "Return 2 values: the category and the first character of the next
+token available in the given STREAM.  Unlike READ-JSON-TOKEN, this
+function can not discriminate between integers and reals (hence, it
+returns a single :NUMBER category), and cannot check whether the next
+available symbol is a valid boolean or not (hence, the category for
+such tokens is :SYMBOL)."
+  (let ((c (peek-char t stream)))
+    (values
+     (case c
+       ((#\{ #\[ #\] #\} #\" #\: #\,) :punct)
+       ((#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\-) :number)
+       ;; Modified to ignore comments.
+       ((#\/)
+        (read-line stream)
+        (json::peek-json-token stream))
+       (t (if (alpha-char-p c)
+              :symbol
+              (json::json-syntax-error stream "Invalid char on JSON input: `~C'"
+                                 c))))
+     c)))
+
 (defmacro with-cl-json-settings (&body body)
   `(let ((json::+json-lisp-symbol-tokens+
            '(("true" . t)
@@ -34,7 +75,8 @@ Objects are transformed to the hash-tables instead.")
          (json:*object-key-handler* #'json-object-add-key)
          (json:*object-value-handler* #'json-object-add-value)
          (json:*end-of-object-handler* #'json-object-get)
-         (json:*json-array-type* 'vector))
+         (json:*json-array-type* 'vector)
+         (json:*use-strict-json-rules* nil))
      ,@body))
 
 (defmethod decode-from-stream ((stream stream))
